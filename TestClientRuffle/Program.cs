9ba64ff007847018cbc70a228798b34461f13082
Enrico Speranza <enrico.speranza@gmail.com>
2019-12-27 16:16:41 +0100

    Second Stub for Client and Server

--- TestClientRuffle/Program.cs
+++ TestClientRuffle/Program.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Net;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-//Ruffles
-using Ruffles.Channeling;
+﻿//Ruffles
 using Ruffles.Configuration;
-using Ruffles.Simulation;
 using Ruffles.Connections;
 using Ruffles.Core;
+
+//System
+using System;
+using System.Net;
+using System.Text;
 
 namespace TestClientRuffle
 {
@@ -34,6 +29,24 @@ namespace TestClientRuffle
         };
 
         static RuffleSocket client = new RuffleSocket(ClientConfig);
+        // The client stores the servers id here
+        static ulong serverId = 0;
+        static Connection serverConnection = null;
+
+        // The time when the connection started
+        DateTime started = DateTime.Now;
+
+        // The time when the last message was sent
+        DateTime lastSent = DateTime.MinValue;
+
+        // The time the last status was printed
+        DateTime lastStatusPrint = DateTime.MinValue;
+
+        // The amount of message that has been received
+        static int messagesReceived = 0;
+
+        // The amount of messages that has been sent
+        static int messageCounter = 0;
 
         /// <summary>
         /// Main Thread
@@ -42,40 +55,74 @@ namespace TestClientRuffle
         static void Main(string[] args)
         {
 
-            // The client stores the servers id here
-            ulong serverId = 0;
-            Connection serverConnection = null;
-
-            // The time when the connection started
-            DateTime started = DateTime.Now;
-
-            // The time when the last message was sent
-            DateTime lastSent = DateTime.MinValue;
-
-            // The time the last status was printed
-            DateTime lastStatusPrint = DateTime.MinValue;
-
-            // The amount of message that has been received
-            int messagesReceived = 0;
-
-            // The amount of messages that has been sent
-            int messageCounter = 0;
-
             try
             {
                 //Star connection
                 client.Start();
-
+                client.OnNetworkEvent += Client_OnNetworkEvent;
+                Console.WriteLine("Connecting...");
                 {
                     // IPv4 Connect
                     client.ConnectNow(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5674));
                 }
+
+                //Start Thread
+                ConsoleKeyInfo cki;
+                // Prevent example from ending if CTL+C is pressed.
+                Console.TreatControlCAsInput = true;
+
+                Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
+                Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+                do
+                {
+                    cki = Console.ReadKey();
+                    Console.Write(" --- You pressed ");
+                    if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+                    if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+                    if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+                    Console.WriteLine(cki.Key.ToString());
+                } while (cki.Key != ConsoleKey.Escape);
+
+                client.Stop();
+                Environment.Exit(0);
             }
             catch (Exception ex)
             {
-
+                Console.Error.WriteLine("Error: " + ex.Message + " from " + ex.Source);
+                client.Shutdown();
+                Environment.Exit(1);
             }
 
         }
+
+        /// <summary>
+        /// Receive NetworkEvent 
+        /// </summary>
+        /// <param name="obj"></param>
+        private static void Client_OnNetworkEvent(NetworkEvent obj)
+        {
+            if (obj.Type != NetworkEventType.Nothing)
+            {
+                if (obj.Type != NetworkEventType.Data)
+                {
+                    Console.WriteLine("ClientEvent: " + obj.Type);
+                }
+
+                if (obj.Type == NetworkEventType.Connect)
+                {
+                    serverId = obj.Connection.Id;
+                    serverConnection = obj.Connection;
+                }
+
+                if (obj.Type == NetworkEventType.Data)
+                {
+                    messagesReceived++;
+                    Console.WriteLine("Got message: \"" + Encoding.ASCII.GetString(obj.Data.Array, obj.Data.Offset, obj.Data.Count) + "\"");
+                }
+            }
+
+            obj.Recycle();
+        }
     }
 }
+
--- TestServerRuffle/Program.cs
+++ TestServerRuffle/Program.cs
@@ -1,15 +1,115 @@
-﻿using System;
+﻿//System
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+//Ruffles
+using Ruffles.Configuration;
+using Ruffles.Channeling;
+using Ruffles.Simulation;
+using Ruffles.Connections;
+using Ruffles.Core;
 
 namespace TestServerRuffle
 {
     class Program
     {
+        internal static readonly SocketConfig ServerConfig = new SocketConfig()
+        {
+            ChallengeDifficulty = 20, // Difficulty 20 is fairly hard
+            ChannelTypes = new ChannelType[]
+           {
+                ChannelType.Reliable,
+                ChannelType.ReliableSequenced,
+                ChannelType.Unreliable,
+                ChannelType.UnreliableOrdered,
+                ChannelType.ReliableSequencedFragmented
+           },
+            DualListenPort = 5674,
+            SimulatorConfig = new Ruffles.Simulation.SimulatorConfig()
+            {
+                DropPercentage = 0.05f,
+                MaxLatency = 10,
+                MinLatency = 0
+            },
+            UseSimulator = false
+        };
+
+        /// <summary>
+        /// Main Thread
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+        }
+
+        /// <summary>
+        /// Return path of main assembly
+        /// </summary>
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        //https://stackoverflow.com/questions/10940883/c-converting-byte-array-to-string-and-printing-out-to-console
+        /// <summary>
+        /// Prints the byte array.
+        /// </summary>
+        /// <param name="bytes">Bytes.</param>
+        public static void PrintByteArray(byte[] bytes)
+        {
+            var sb = new StringBuilder("new byte[] { ");
+            foreach (var b in bytes)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+            Console.WriteLine(sb.ToString());
+        }
+
+        //https://stackoverflow.com/posts/9543797/revisions
+        //https://stackoverflow.com/questions/9543715/generating-human-readable-usable-short-but-unique-ids?answertab=votes#tab-top
+        /// <summary>
+        /// Random identifier generator.
+        /// </summary>
+        public static class RandomIdGenerator
+        {
+            private static char[] _base62chars =
+                "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                .ToCharArray();
+
+            private static Random _random = new Random();
+
+            public static string GetBase62(int length)
+            {
+                var sb = new StringBuilder(length);
+
+                for (int i = 0; i < length; i++)
+                    sb.Append(_base62chars[_random.Next(62)]);
+
+                return sb.ToString();
+            }
+
+            public static string GetBase36(int length)
+            {
+                var sb = new StringBuilder(length);
+
+                for (int i = 0; i < length; i++)
+                    sb.Append(_base62chars[_random.Next(36)]);
+
+                return sb.ToString();
+            }
         }
     }
 }
